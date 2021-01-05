5140424b693c98c9485e43dc9f5665753e5f2028
John Villalvazo <jvillalvazo2@ucmerced.edu>
2021-01-04 17:54:44 -0800

    problem 5

---
+++ Prompt_5/5.py
@@ -0,0 +1,16 @@
+
+#! 5) Specify the “2nd" largest number between 10 numbers
+
+elements = int(input("Enter the size of your list: "))
+
+mainArr = []
+
+for i in range(0, elements):
+    print("Enter a number")
+    arr = int(input())
+
+    mainArr.append(arr)
+
+
+
+print(mainArr)
