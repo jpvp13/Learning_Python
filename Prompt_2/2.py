0aa9c4c712c3f7f7e9cc955203e743f371afca60
John Villalvazo <jvillalvazo2@ucmerced.edu>
2021-01-04 17:54:26 -0800

    problem 2

--- Prompt_2/2.py
+++ Prompt_2/2.py
@@ -1,15 +1,15 @@
 
 #! 2) Add 2 numbers entered by the user
 
-num1 = input("Please enter a number of your choosing:" )
+num1 = int(input("Please enter a number of your choosing:" ))
+        #~ typecase the input into a int so that the addition can be done later
 
 print("Num1 is equal to = : ", num1)
 
-num2 = input("Please enter another number of your choosing:" )
+num2 = int(input("Please enter another number of your choosing:" ))
+        #~ typecase the input into a int so that the addition can be done later
+
 
 print("Num2 is equal to = : ", num2)
 
 print("The sum of these two numbers is : ", num1 + num2)
-
-
-
